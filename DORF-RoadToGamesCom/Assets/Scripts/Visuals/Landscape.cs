82559bedb35ad1fb8eacefc005dfaf14a9011de5
MarvinWack <marc.uhlherr@gmail.com>
2026-06-19 12:16:48 +0200

    fixing expression stuff, slowdown for landscape scene 1

--- DORF-RoadToGamesCom/Assets/Scripts/Visuals/Landscape.cs
+++ DORF-RoadToGamesCom/Assets/Scripts/Visuals/Landscape.cs
@@ -1,19 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class Landscape : MonoBehaviour
     {
-        [SerializeField,Range(0.01f, 1)] private float speed;
+        [Header("Settings")]
+        [SerializeField, Range(0f, 1f)] private float speed;
+        [SerializeField] private float slowDownDuration;
+        
         [SerializeField] private MeshRenderer meshRenderer;
-        //Texture2D und wrap mode: repeat
 
         private Material material;
+        private Coroutine slowDownCoroutine;
 
         private void Start()
         {
             meshRenderer = GetComponent<MeshRenderer>();
-            
             material = meshRenderer.material;
         }
 
@@ -26,8 +29,34 @@ namespace DefaultNamespace
         {
             Vector2 offset = material.mainTextureOffset;
             offset.x += speed * Time.deltaTime;
-            
+
             material.mainTextureOffset = offset;
+        }
+
+        [ContextMenu("Slow Down")]
+        public void SlowDown()
+        {
+            if (slowDownCoroutine != null)
+            {
+                StopCoroutine(slowDownCoroutine);
+            }
+            
+            slowDownCoroutine = StartCoroutine(SlowDownRoutine());
+        }
+        
+        private IEnumerator SlowDownRoutine()
+        {
+            float startSpeed = speed;
+            float timeElapsed = 0f;
+
+            while (timeElapsed < slowDownDuration)
+            {
+                speed = Mathf.Lerp(startSpeed, 0f, timeElapsed / slowDownDuration);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            speed = 0f;
         }
     }
 }
